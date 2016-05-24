13ed8deab8506d428cfbffd5cfb12803266bc16a
Boris <boris@sclable.com>
2016-05-24 09:47:18 +0200

    bean added - slackComponent slack -> returns the webhook URL

--- src/main/java/at/ac/tuwien/wmpm/App.java
+++ src/main/java/at/ac/tuwien/wmpm/App.java
@@ -9,6 +9,7 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.component.slack.SlackComponent;
 import org.apache.camel.component.spring.ws.bean.CamelEndpointMapping;
 import org.apache.camel.spring.boot.CamelSpringBootApplicationController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,6 +96,13 @@ public class App extends RepositoryRestMvcConfiguration {
 		return new CamelEndpointMapping();
 	}
 
+	@Bean
+	public SlackComponent slack() {
+		SlackComponent sc = new SlackComponent();
+		sc.setWebhookUrl("https://hooks.slack.com/services/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");
+		return sc;
+	}
+	
 	@Bean
 	@ConditionalOnClass(value = DataSource.class)
 	public DataSource dataSource() {
